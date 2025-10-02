79de66fa6d14466b43008446d467ca334c8a23b7
carlosjimenezmoreno59-debug <carlosjimenezmoreno59@gmail.com>
2025-10-02 12:43:53 +0200

    Todos los cambios respecto a las primeras lineas de java

--- ProyectoInicialtema 1/src/Main.java
+++ ProyectoInicialtema 1/src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
-}
+
+    public static void main (String[] args){
+System.out.println("Este es mi primer proyecto en Java");
+System.out.println("Esta la segunda linea");
+System.out.println("Esta es la tercera linea");
+System.out.println("Esta es la cuarta linea");
+System.out.println("Esta es la quinta linea");
+System.out.println("Esta es la sexta linea");
+System.out.println("Esta es la septima linea");
+System.out.println(6);
+System.out.println(8<9); }}
